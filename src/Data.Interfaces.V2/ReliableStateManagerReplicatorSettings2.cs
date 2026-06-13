1c0f356adf8eb1fee5e73f726a8ee5e15513d792
Oleg Sych <olegsych@users.noreply.github.com>
2026-06-13 12:14:47 -0700

    squash! src/Data.Interfaces.V2/ReliableStateManagerReplicatorSettings2.cs
    Address the following finding.
    
    Document default values via a <value> element
    
    Defaults are currently embedded at the end of each property <summary>. Per
    docs.instructions.md, defaults belong in a separate <value> sentence (e.g. The
    default is <see langword="false" />.). Move each default into <value>:
    CopyBatchSizeInKB=0, EnableStableReads=false, ShouldAbortCopyForTruncation=false,
    ReplicationBatchSize=1, ReplicationBatchSendInterval=15 ms.

--- src/Data.Interfaces.V2/ReliableStateManagerReplicatorSettings2.cs
+++ src/Data.Interfaces.V2/ReliableStateManagerReplicatorSettings2.cs
@@ -15,32 +15,42 @@ namespace Microsoft.ServiceFabric.Data
     {
         /// <summary>
         /// Gets or sets the size, in kilobytes, of the copy log message used to build a replica. A higher value copies more log records in each message.
-        /// The default is 0, which copies one log record at a time.
         /// </summary>
+        /// <value>
+        /// The default is 0, which copies one log record at a time.
+        /// </value>
         public long? CopyBatchSizeInKB { get; set; }
 
         /// <summary>
         /// Gets or sets a value that indicates whether stable reads are enabled. Stable reads allow every replica to return only values that are quorum acknowledged on a read.
-        /// The default is <see langword="false" />.
         /// </summary>
+        /// <value>
+        /// The default is <see langword="false" />.
+        /// </value>
         public bool? EnableStableReads { get; set; }
 
         /// <summary>
         /// Gets or sets a value that indicates whether a build can be canceled when the log is full.
-        /// The default is <see langword="false" />.
         /// </summary>
+        /// <value>
+        /// The default is <see langword="false" />.
+        /// </value>
         public bool? ShouldAbortCopyForTruncation { get; set; }
         
         /// <summary>
         /// Gets or sets the number of operations in a replication batch.
-        /// The default is 1.
         /// </summary>
+        /// <value>
+        /// The default is 1.
+        /// </value>
         public long? ReplicationBatchSize { get; set; }
 
         /// <summary>
         /// Gets or sets the interval at which a replication batch is force sent even if it hasn't reached <see cref="ReplicationBatchSize" />.
-        /// The default is 0.015 seconds (15 milliseconds).
         /// </summary>
+        /// <value>
+        /// The default is 0.015 seconds (15 milliseconds).
+        /// </value>
         public TimeSpan? ReplicationBatchSendInterval { get; set; }
 
         /// <summary>
